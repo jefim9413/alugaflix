9e95d70ad60cf54265e4b02fb6a3d8cd105919dc
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-11-30 15:47:54 -0300

    repositorio de filmes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,12 @@
 import { container } from 'tsyringe'
 import { IUserRepository } from '../../modules/user/repositories/IUserRepository'
 import { UserRepository } from '../../modules/user/infra/prisma/repositories/UserRepository'
+import { IFilmeRepository } from '../../modules/filme/repositories/IFilmeRepository'
+import { FilmeRepository } from '../../modules/filme/infra/prisma/repositories/FilmeRepository'
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+
+container.registerSingleton<IFilmeRepository>(
+  'FilmeRepository',
+  FilmeRepository,
+)
